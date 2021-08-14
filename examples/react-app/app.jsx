a93e2b9c68232507f7cc48d2bc7dbdf0f353c39f
X <i@jex.me>
2021-08-14 22:39:22 +0800

    Update examples

---
+++ examples/react-app/app.jsx
@@ -0,0 +1,8 @@
+import React from 'https://esm.sh/react@17'
+import {render} from 'https://esm.sh/react-dom@17'
+
+const App = () => {
+  return <h1>Hello World!</h1>
+}
+
+render(<App />, document.querySelector('#root'))
